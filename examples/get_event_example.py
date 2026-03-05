ade0d78f2acf457b2e1f637d9847fa8ded91ae5b
Eray Aydın <eray.aydin@fingerprint.com>
2026-03-05 20:00:01 +0300

    docs: add ruleset evaluation example to get_event
    Adds ruleset evaluation example to the `get_event_example` example file.
    
    Related-Task: INTER-1799

--- examples/get_event_example.py
+++ examples/get_event_example.py
@@ -10,15 +10,26 @@ load_dotenv()
 
 # configure
 region_str = os.environ.get("REGION", "us").upper()
+api_key = os.environ.get("PRIVATE_KEY")
+
+if not api_key:
+    print("API key not provided")
+    exit(1)
+
 configuration = fingerprint_server_sdk.Configuration(
-    api_key=os.environ["PRIVATE_KEY"], region=Region[region_str])
+    api_key=api_key, region=Region[region_str])
 
 # create an instance
 api_instance = fingerprint_server_sdk.FingerprintApi(configuration)
-event_id = os.environ["EVENT_ID"]
+event_id = os.environ.get("EVENT_ID", None)
+ruleset_id = os.environ.get("RULESET_ID", None)
+
+if not event_id:
+    print("Event id not provided")
+    exit(1)
 
 try:
-    event = api_instance.get_event(event_id)
+    event = api_instance.get_event(event_id, ruleset_id)
     print(f"Event ID: {event.event_id}")
     print(f"Timestamp: {event.timestamp}")
     if event.identification:
@@ -26,6 +37,15 @@ try:
         print(f"Confidence: {event.identification.confidence.score}")
     if event.bot:
         print(f"Bot detection result: {event.bot}")
+    if ruleset_id and event.rule_action:
+        rule_action = event.rule_action.actual_instance
+        print(f"Rule action: {rule_action.type}")
+        if rule_action.type == 'block':
+            print(f"Block with HTTP status code: `{rule_action.status_code}`")
+            print(f"Block with response body: `{rule_action.body}`")
+            print(f"Block with response headers: `{rule_action.headers}`")
+        elif rule_action.type == 'allow':
+            print(f"Allow with header modifications: `{rule_action.request_header_modifications}`")
 except ApiException as e:
     print("Exception when calling get_event: %s\n" % e)
     exit(1)
